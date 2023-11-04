d5d580e48009450c809770506baa8cf132c4a964
Jack <jackw221001@Gmail.com>
2023-11-04 09:58:56 +0000

    chore: ESLint rules

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,7 @@
 import { describe, it, expect } from 'vitest';
+import { Path } from '$env/static/private';
+
+console.log(Path);
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
